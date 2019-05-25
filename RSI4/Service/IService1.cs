06d7ea96579827ad4aa5960beeb3b6fac1ae685e
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2019-05-25 11:50:02 +0200

    zmiany zmiany zmiany

--- RSI4/Service/IService1.cs
+++ RSI4/Service/IService1.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Text;
 
-namespace Serwis
+namespace Service
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
@@ -18,19 +17,12 @@ namespace Serwis
         [OperationContract(IsOneWay = true)]
         void Funkcja2(String s1);
 
-
-
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
+       
         // TODO: Add your service operations here
     }
 
-
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Service.ContractType".
     [DataContract]
     public class CompositeType
     {
---
+++ RSI4/Service/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public void Funkcja1(string s1)
+        {
+            Console.WriteLine("...{ 0} fkcja1 - start");
+            Thread.Sleep(3000);
+            Console.WriteLine("...{ 0} fkcja1 - stop");
+            return;
+        }
+
+        public void Funkcja2(string s1)
+        {
+            Console.WriteLine("...{ 0} fkcja2 - start");
+            Thread.Sleep(3000);
+            Console.WriteLine("...{ 0} fkcja2 - stop");            
+            return;
+        }
+
+      
+    }
+}
