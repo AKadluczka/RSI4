82c27fa1c6e7e5ae06f4bd341c25727702853961
AKadluczka <49155728+AKadluczka@users.noreply.github.com>
2019-05-26 16:01:13 +0200

    interfejs

--- RSI4/Klient_graficzny/Form1.Designer.cs
+++ RSI4/Klient_graficzny/Form1.Designer.cs
@@ -1,6 +1,10 @@
-﻿namespace Klient_graficzny
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Klient_graficzny
 {
-    partial class Form1
+    partial class Form1 : Form
     {
         /// <summary>
         /// Required designer variable.
@@ -26,15 +30,145 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        /// 
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+
+     
+
+
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "pobierz listę obrazków";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(339, 22);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(265, 24);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(339, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 223);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 303);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(186, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "lista obrazów";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(28, 39);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(186, 293);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
-        #endregion
+        
+
+
+        public static int interwal = 0;
+
+        private void timer1_Tick(object sander, EventArgs e)
+        {
+            interwal += 1;
+            progressBar1.Increment(interwal);
+        }
+
+
+        private void button1_Click(object sander, EventArgs e)
+        {
+            //ladowanie listy obrazów
+        }
+
+        private void checkedListBox1_Click(object sander, EventArgs e)
+        {
+            timer1.Start();
+            /* trzeba wyswietlic odpowiednie zdj*/
+            checkedListBox1.CheckedItems.
+            for (int i = 0; i < checkedListBox1.Che; i++)
+
+
+                string path=//sciezka z nacisnietego zdj
+                string text = tekst z nacisnietego zdj
+
+
+                    
+
+                pictureBox1.Image = Image.FromFile(path);
+            textBox1.Text = text;
+              
+            if (progressBar1.Value > progressBar1.Maximum)
+            {
+                progressBar1.Value = progressBar1.Maximum;
+            }
+            else
+            {
+                timer1.Stop();
+
+            }
+        }
     }
 }
 
--- RSI4/Klient_graficzny/Form1.cs
+++ RSI4/Klient_graficzny/Form1.cs
@@ -16,5 +16,10 @@ namespace Klient_graficzny
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
